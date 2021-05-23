fc2d6a941efe9077b7d31f1c32a78a6e3bb8fcf4
Vicente Silva <vicente.pspenha@gmail.com>
2021-05-23 19:59:44 -0300

    Commit inicial.

---
+++ src/main/java/com/vicente/teste/TesteEscolaIdsApplication.java
@@ -0,0 +1,13 @@
+package com.vicente.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteEscolaIdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteEscolaIdsApplication.class, args);
+	}
+
+}
