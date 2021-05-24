dd66d43df7eec64a06d39d464443f974677e790f
Vicente Silva <vicente.pspenha@gmail.com>
2021-05-24 18:23:13 -0300

    Habilitado cors para requisição local do Angular.

--- src/main/java/com/vicente/teste/controllers/AvaliacaoController.java
+++ src/main/java/com/vicente/teste/controllers/AvaliacaoController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import com.vicente.teste.services.AvaliacaoService;
 
 import javassist.NotFoundException;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/avaliacoes")
 public class AvaliacaoController {
--- src/main/java/com/vicente/teste/controllers/AvaliacaoPesoController.java
+++ src/main/java/com/vicente/teste/controllers/AvaliacaoPesoController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.vicente.teste.services.AvaliacaoPesoService;
 
 import javassist.NotFoundException;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/avaliacoes-peso")
 public class AvaliacaoPesoController {
