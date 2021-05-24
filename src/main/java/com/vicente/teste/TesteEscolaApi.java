2d27ef02547c0dee9731600e4810b698c42d7b38
Vicente Silva <vicente.pspenha@gmail.com>
2021-05-24 08:39:47 -0300

    Renomeado classe principal.

--- src/main/java/com/vicente/teste/TesteEscolaApi.java
+++ src/main/java/com/vicente/teste/TesteEscolaApi.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TesteEscolaIdsApplication {
+public class TesteEscolaApi {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TesteEscolaIdsApplication.class, args);
+		SpringApplication.run(TesteEscolaApi.class, args);
 	}
 
 }
