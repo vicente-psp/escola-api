d60cf017e7a9b501b24a22b556226be70697f2c5
Vicente Silva <vicente.pspenha@gmail.com>
2021-05-24 10:41:29 -0300

    Mapeado método para salvar recursos.

--- src/main/java/com/vicente/teste/controllers/AvaliacaoController.java
+++ src/main/java/com/vicente/teste/controllers/AvaliacaoController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,7 @@ public class AvaliacaoController {
 		}
 	}
 	
+	@PostMapping
 	public ResponseEntity<?> save(@Valid @RequestBody Avaliacao entity) throws Exception {
 		try {
 			return ResponseEntity.ok(service.save(entity));
